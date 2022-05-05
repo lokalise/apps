6526e14b8144c6a06a4a45374217eeb976fc45df
bodrovis <bodrovis@protonmail.com>
2022-05-05 18:17:54 +0300

    prettify

--- examples/webhooks-node-express/controllers/webhooksController.js
+++ examples/webhooks-node-express/controllers/webhooksController.js
@@ -3,10 +3,10 @@ import { ApplicationController } from "./application_controller.js"
 export class WebhooksController extends ApplicationController { 
   static async create(req, res) {
     try {
-    await this.lokaliseApi().webhooks().create(
-      {url: process.env.NOTIFY_URL, events: ['project.key.added']},
-      {project_id: req.body.project_id}
-    );
+      await this.lokaliseApi().webhooks().create(
+        {url: process.env.NOTIFY_URL, events: ['project.key.added']},
+        {project_id: req.body.project_id}
+      )
     } catch(e) {
       console.error(e)
     }
