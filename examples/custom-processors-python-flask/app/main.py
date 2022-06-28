23fb3c5b293e299aa5a2e2a81ebea7fc016501d3
bodrovis <bodrovis@protonmail.com>
2022-06-28 16:43:31 +0300

    flask

--- examples/custom-processors-python-flask/app/main.py
+++ examples/custom-processors-python-flask/app/main.py
@@ -1,2 +1,25 @@
+import re
 from flask import Flask, request
 
+app = Flask(__name__)
+
+@app.route('/preprocess', methods=['POST'])
+def preprocess():
+    data = request.get_json()
+
+    for keyId, key in enumerate(data['collection']['keys']):
+        for transId, trans in enumerate(key['translations']):
+            data['collection']['keys'][keyId]['translations'][transId]['translation'] = re.sub('BANNED', '', trans['translation'])
+
+    return data, 200
+
+
+@app.route('/postprocess', methods=['POST'])
+def postprocess():
+    data = request.get_json()
+
+    for keyId, key in enumerate(data['collection']['keys']):
+        for transId, trans in enumerate(key['translations']):
+            data['collection']['keys'][keyId]['translations'][transId]['translation'] = re.sub('BANNED', '', trans['translation'])
+
+    return data, 200
