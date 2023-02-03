143e5d861803821f5d5435bb2f87434dc50ed058
CarlosGamero <carlos.gamero@lokalise.com>
2023-02-03 16:38:06 +0100

    DP-166 Removing project id and token

--- examples/flutter-ota/lib/main.dart
+++ examples/flutter-ota/lib/main.dart
@@ -5,8 +5,8 @@ import 'package:to_do_app/app/app.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Lokalise.init(
-    sdkToken: 'd24a6c4e93076b29e71347e8c39713f87f1e',
-    projectId: '595412416388cd7cdebfe5.92023764',
+    projectId: '', // Fill with your project id
+    sdkToken: '', // Fill with your SDK token
   );
   runApp(const App());
 }
